8fc5f77f628c69e37f44159af3d7b857b4a9f235
Jinsu Park <parkjinsu4755@gmail.com>
2023-01-03 14:57:24 +0900

    Ci/lint build check (#4)
    * build: Add Ktlint gradle plugin
    
    * style: ktlint style로 코드 수정
    
    * ci(check): Create ktlint, build, test check CI 생성
    
    * ci(check): Branch Push Build Checker 제작
    
    * ci(check): CI Check 조건 브랜치 이름 변경
    
    * ci(check): CI Check 조건 push 로 변경
    
    * ci(check): CI Slack 알림 프로필 변경

--- app/src/main/java/org/sopt/zooczoocbbangbbang/data/ServiceFactory.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/data/ServiceFactory.kt
@@ -1,8 +1,5 @@
 package org.sopt.zooczoocbbangbbang.data
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
