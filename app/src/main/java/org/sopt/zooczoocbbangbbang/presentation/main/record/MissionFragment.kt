e31557b29e8810afe7354c049f43c6aa245956c2
Cat-JiYoung <kimyamae82@gmail.com>
2023-01-04 19:08:04 +0900

    feat(record): Record fragment &  Mission fragment 생성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/MissionFragment.kt
@@ -0,0 +1,29 @@
+package org.sopt.zooczoocbbangbbang.presentation.main.record
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.sopt.zooczoocbbangbbang.databinding.FragmentMissionBinding
+
+class MissionFragment : Fragment() {
+    private var _binding: FragmentMissionBinding? = null
+    private val binding get() = requireNotNull(_binding) { }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMissionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+    }
+}
