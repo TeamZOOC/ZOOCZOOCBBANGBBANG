25abeff69d3a1c0047b12df9fca8b09a6fd1a496
Cat-JiYoung <kimyamae82@gmail.com>
2023-01-06 04:26:38 +0900

    feat(choosePet): 반려동물 선택 뷰 (2,3,4개) xml 구현

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/Choose3PetFragment.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/Choose3PetFragment.kt
@@ -4,7 +4,8 @@ import org.sopt.zooczoocbbangbbang.R
 import org.sopt.zooczoocbbangbbang.databinding.FragmentChoose3PetBinding
 import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
 
-class Choose3PetFragment : BindingFragment<FragmentChoose3PetBinding>(R.layout.fragment_choose3_pet) {
+class Choose3PetFragment :
+    BindingFragment<FragmentChoose3PetBinding>(R.layout.fragment_choose3_pet) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
