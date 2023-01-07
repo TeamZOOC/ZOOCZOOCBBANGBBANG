37402b0bc4049327d6b95e19b7bb932c82bc5e04
Cat-JiYoung <kimyamae82@gmail.com>
2023-01-07 18:47:29 +0900

    feat(viewmodel): mission viewmodel 생성 및 연결

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/MissionFragment.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/MissionFragment.kt
@@ -1,17 +1,24 @@
 package org.sopt.zooczoocbbangbbang.presentation.main.record
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.viewModels
 import coil.load
 import org.sopt.zooczoocbbangbbang.R
 import org.sopt.zooczoocbbangbbang.databinding.FragmentMissionBinding
 import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
 
 class MissionFragment : BindingFragment<FragmentMissionBinding>(R.layout.fragment_mission) {
+    private val missionViewModel: MissionViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewmodel = missionViewModel
         clickImageBtn()
+        clickNextBtn()
+        observe()
     }
 
     // 사진을 불러오는 함수
@@ -19,10 +26,29 @@ class MissionFragment : BindingFragment<FragmentMissionBinding>(R.layout.fragmen
         val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) {
             binding.ivMissionBgimg.load(it) {
                 transformations()
+                missionViewModel.image.value = it
             }
         }
         binding.ivMissionImgbtn.setOnClickListener {
             launcher.launch("image/*")
+        }
+    }
+
+    private fun clickNextBtn() {
+        binding.btnMissionBottom.setOnClickListener() {
+            Log.d("MissionFragment", "반려동물 선택 뷰로 이동하는 로직 필요!")
+        }
+    }
+
+    private fun observe() {
+        missionViewModel.image.observe(viewLifecycleOwner) {
+            Log.d("MissionFragment", "image:::${missionViewModel.image.value}")
+        }
+        missionViewModel.missionText.observe(viewLifecycleOwner) {
+            Log.d("MissionFragment", "Recordtext:::$it")
+        }
+        missionViewModel.buttonValidation.observe(viewLifecycleOwner) {
+            Log.d("MissionFragment", "validation:::$it")
         }
     }
 
