d8b55aecae7eb11399e92469d3a6b447e4fded0c
Cat-JiYoung <kimyamae82@gmail.com>
2023-01-07 16:30:57 +0900

    feat(mission): mission fragment 생성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/record/MissionFragment.kt
@@ -0,0 +1,11 @@
+package org.sopt.zooczoocbbangbbang.presentation.main.record
+
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentMissionBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class MissionFragment : BindingFragment<FragmentMissionBinding>(R.layout.fragment_mission) {
+
+    companion object {
+    }
+}
