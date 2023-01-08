f82d35e85511f5074095b435c4fd25e81abf984c
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-09 08:25:40 +0900

    feat(onboard): Create ProfileCreateActivity

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/ProfileCreateActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.zooczoocbbangbbang.presentation.main.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.zooczoocbbangbbang.R
+
+class ProfileCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_create)
+    }
+}
