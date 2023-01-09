1a93cbdb17964d73d388b519dc09fb68913eb00b
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-09 10:05:16 +0900

    feat(onboard): Create RoleInputFragment

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/profile/RoleInputFragment.kt
@@ -0,0 +1,9 @@
+package org.sopt.zooczoocbbangbbang.presentation.main.onboarding.profile
+
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentRoleInputBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class RoleInputFragment : BindingFragment<FragmentRoleInputBinding>(R.layout.fragment_role_input) {
+
+}
