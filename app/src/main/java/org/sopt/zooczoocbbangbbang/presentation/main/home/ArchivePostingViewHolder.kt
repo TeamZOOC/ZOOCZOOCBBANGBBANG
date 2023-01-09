382670a494a76be59e48c3dcecc7098f2bab6bc6
brian <briandr@naver.com>
2023-01-10 00:32:08 +0900

    feat(archive): 클릭 리스너 위치 onBindViewHolder에서 ViewHolder로 옮김

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/home/ArchivePostingViewHolder.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/home/ArchivePostingViewHolder.kt
@@ -1,55 +1,56 @@
 package org.sopt.zooczoocbbangbbang.presentation.main.home
 
-import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import org.sopt.zooczoocbbangbbang.databinding.ItemGridArchivePostingBinding
 import org.sopt.zooczoocbbangbbang.databinding.ItemLinearArchivePostingBinding
 import org.sopt.zooczoocbbangbbang.domain.ArchivePostingData
-import org.sopt.zooczoocbbangbbang.util.GlideExt
 
 abstract class ArchivePostingViewHolder(private val binding: View) :
     RecyclerView.ViewHolder(binding) {
-    abstract fun onBind(data: ArchivePostingData)
+    abstract fun onBind(data: ArchivePostingData, clickItem: () -> Unit)
 }
 
 class ArchivePostingLinearViewHolder(private val binding: ItemLinearArchivePostingBinding) :
     ArchivePostingViewHolder(binding.root) {
 
-    override fun onBind(data: ArchivePostingData) {
-        GlideExt.loadIntImage(data.petImage, binding.ivArchivePetImage)
-        GlideExt.loadIntImage(data.editorImage, binding.ivArchiveUploaderProfile)
+    private lateinit var commentersAdapter: CommentersAdapter
+
+    override fun onBind(data: ArchivePostingData, clickItem: () -> Unit) {
+        binding.ivArchivePetImage.load(data.petImage)
+        binding.ivArchiveUploaderProfile.load(data.editorImage)
         binding.data = data
+        initAdapter(data.commenters)
+
         if (data.isSelected) {
             expandItem()
-            Log.d("asdf", "expand / end state: ${binding.mlArchivePosting.endState}")
         } else {
             foldItem()
-            Log.d("asdf", "fold / start state: ${binding.mlArchivePosting.startState}")
+        }
+
+        itemView.setOnClickListener {
+            clickItem()
         }
     }
 
-    fun updateUi(foldableUiState: FoldableUiState) {
-        when (foldableUiState) {
-            FoldableUiState.EXPAND -> expandItem()
-            FoldableUiState.FOLD -> foldItem()
-        }
+    private fun initAdapter(commenters: List<ArchivePostingData.Commenter>) {
+        commentersAdapter = CommentersAdapter(commenters)
+        binding.rvCommenters.adapter = commentersAdapter
     }
 
     private fun expandItem() {
         binding.mlArchivePosting.transitionToEnd()
-        Log.d("asdf", "확장됨")
     }
 
     private fun foldItem() {
         binding.mlArchivePosting.transitionToStart()
-        Log.d("asdf", "접기")
     }
 }
 
 class ArchivePostingGridViewHolder(private val binding: ItemGridArchivePostingBinding) :
     ArchivePostingViewHolder(binding.root) {
-    override fun onBind(data: ArchivePostingData) {
-        GlideExt.loadIntImage(data.petImage, binding.ivArchiveGridPetImage)
+    override fun onBind(data: ArchivePostingData, clickItem: () -> Unit) {
+        binding.ivArchiveGridPetImage.load(data.petImage)
     }
 }
