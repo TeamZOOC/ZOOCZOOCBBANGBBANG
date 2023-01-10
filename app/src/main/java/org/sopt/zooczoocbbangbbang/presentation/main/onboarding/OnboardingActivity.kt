a594cbc5269627825d57fbf41d1242bfc9994dc7
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-10 10:07:30 +0900

    feat(onboard): onboarding Activity 이름 변경

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/OnboardingActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.zooczoocbbangbbang.R
 
-class ProfileCreateActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_create)
+        setContentView(R.layout.activity_onboarding)
     }
 }
