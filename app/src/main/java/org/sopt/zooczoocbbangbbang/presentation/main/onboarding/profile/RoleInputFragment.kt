1ed1ccfa4f99a052f5cc1ec2f3a93bd8aa5d23ff
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-10 12:03:57 +0900

    feat(onboard): 역할 입력 fragment 이름 변경

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/profile/RoleInputFragment.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/main/onboarding/profile/RoleInputFragment.kt
@@ -1,9 +1,8 @@
 package org.sopt.zooczoocbbangbbang.presentation.main.onboarding.profile
 
 import org.sopt.zooczoocbbangbbang.R
-import org.sopt.zooczoocbbangbbang.databinding.FragmentRoleInputBinding
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingRoleInputBinding
 import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
 
-class RoleInputFragment : BindingFragment<FragmentRoleInputBinding>(R.layout.fragment_role_input) {
-
-}
+class RoleInputFragment :
+    BindingFragment<FragmentOnboardingRoleInputBinding>(R.layout.fragment_onboarding_role_input)
