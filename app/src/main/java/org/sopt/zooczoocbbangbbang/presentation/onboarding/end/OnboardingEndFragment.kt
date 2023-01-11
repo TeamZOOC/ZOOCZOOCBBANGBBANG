3a72056031da7afd96a76df9c4307c9d7c865ad6
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:58:57 +0900

    feat(onboard): 온보딩 마무리 프래그먼트 작성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/end/OnboardingEndFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.end
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingEndBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class OnboardingEndFragment : BindingFragment<FragmentOnboardingEndBinding>(R.layout.fragment_onboarding_end) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
