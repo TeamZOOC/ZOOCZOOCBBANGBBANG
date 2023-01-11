f5e22948dc677a7c1c94181e757221daa8fb2b41
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:59:45 +0900

    feat(onboard): 가족초대 이전 프래그먼트 작성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/invitation/InvitationBeforeFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.invitation
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingInvitationShareLinkBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class InvitationBeforeFragment : BindingFragment<FragmentOnboardingInvitationShareLinkBinding>(R.layout.fragment_onboarding_invitation_share_link) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
