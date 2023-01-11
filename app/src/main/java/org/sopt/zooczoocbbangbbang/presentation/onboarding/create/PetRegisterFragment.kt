7463591df51e3e3b9e493798909316040b9b1d49
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:58:43 +0900

    feat(onboard): 반려동물 등록 프래그먼트 작성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/create/PetRegisterFragment.kt
@@ -0,0 +1,14 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.create
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingPetRegisterBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class PetRegisterFragment :
+    BindingFragment<FragmentOnboardingPetRegisterBinding>(R.layout.fragment_onboarding_pet_register) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
