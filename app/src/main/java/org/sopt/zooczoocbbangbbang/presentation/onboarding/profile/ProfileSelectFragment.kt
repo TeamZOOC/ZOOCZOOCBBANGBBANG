5cc3947c7953a579a879d36cd9431228518eda65
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:58:27 +0900

    feat(onboard): 프로필 선택 fragment 생성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/profile/ProfileSelectFragment.kt
@@ -0,0 +1,14 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.profile
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingProfileSelectorBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class ProfileSelectFragment :
+    BindingFragment<FragmentOnboardingProfileSelectorBinding>(R.layout.fragment_onboarding_profile_selector) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
