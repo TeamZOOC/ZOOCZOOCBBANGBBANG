37528e5d2556452bb47c7dca7390769043c318e5
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:59:07 +0900

    feat(onboard): 가족코드 입력 프래그먼트 작성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/join/JoinFamilyFragment.kt
@@ -0,0 +1,14 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.join
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingInputCodeBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class JoinFamilyFragment :
+    BindingFragment<FragmentOnboardingInputCodeBinding>(R.layout.fragment_onboarding_input_code) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
