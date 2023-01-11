36dd91fe6f69899076946f24fafeef4aceec53b6
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-12 07:59:23 +0900

    feat(onboard): 프로필 작성 완료 프래그먼트 작성

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/profile/CompleteProfileFragment.kt
@@ -0,0 +1,14 @@
+package org.sopt.zooczoocbbangbbang.presentation.onboarding.profile
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.zooczoocbbangbbang.R
+import org.sopt.zooczoocbbangbbang.databinding.FragmentOnboardingProfileCompleteBinding
+import org.sopt.zooczoocbbangbbang.presentation.base.BindingFragment
+
+class CompleteProfileFragment :
+    BindingFragment<FragmentOnboardingProfileCompleteBinding>(R.layout.fragment_onboarding_profile_complete) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
