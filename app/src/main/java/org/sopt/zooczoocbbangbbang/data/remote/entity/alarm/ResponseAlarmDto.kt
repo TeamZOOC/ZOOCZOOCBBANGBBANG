2e2d4b33e44914353684f6eb54cfa303e772150c
brian <briandr@naver.com>
2023-01-12 14:28:45 +0900

    feat(alarm): 알림 레이아웃 및 서버통신 구현

---
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/data/remote/entity/alarm/ResponseAlarmDto.kt
@@ -0,0 +1,23 @@
+package org.sopt.zooczoocbbangbbang.data.remote.entity.alarm
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseAlarmDto(
+    val data: List<Alarm>,
+    val message: String,
+    val status: Int,
+    val success: Boolean
+) {
+    data class Alarm(
+        @SerializedName("created_time")
+        val createdTime: String,
+        val writer: Writer
+    ) {
+        data class Writer(
+            val id: Int,
+            @SerializedName("nick_name")
+            val nickName: String,
+            val photo: String
+        )
+    }
+}
