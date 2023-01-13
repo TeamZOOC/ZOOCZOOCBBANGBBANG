bebc1e8686b53c78e27f038d26b309e201ac5a5d
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-14 05:37:02 +0900

    feat(onboarding): 온보딩에서 사용할 repository 작성

--- app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/OnboardingRepository.kt
+++ app/src/main/java/org/sopt/zooczoocbbangbbang/presentation/onboarding/OnboardingRepository.kt
@@ -1,3 +1,14 @@
 package org.sopt.zooczoocbbangbbang.presentation.onboarding
 
-class OnboardingRepository
+class OnboardingRepository private constructor() {
+    companion object {
+        @Volatile
+        private var instance: OnboardingRepository? = null
+
+        fun getInstance() = instance ?: synchronized(this) {
+            instance ?: OnboardingRepository().apply {
+                instance = this
+            }
+        }
+    }
+}
